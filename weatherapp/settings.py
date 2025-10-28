30e7b90829dafc6dfb89be2ffaa2c90385e746ec
Yael Tapia <90061475+yberside42@users.noreply.github.com>
2025-10-28 17:26:04 -0600

    Update settings.py

--- weatherapp/settings.py
+++ weatherapp/settings.py
@@ -130,12 +130,6 @@ STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"   
 STATICFILES_DIRS = [BASE_DIR / "static"] 
 
-STORAGES = {
-    "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage"
-    }
-}
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
